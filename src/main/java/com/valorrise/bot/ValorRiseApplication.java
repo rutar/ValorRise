9ae45311d008fb4f513ff6d5ed3a3b7b7fa3088f
rutar <rustarg@gmail.com>
2025-05-26 13:13:40 +0300

    Set up a new Spring Boot application named "ValorRise" using Gradle.

---
+++ src/main/java/com/valorrise/bot/ValorRiseApplication.java
@@ -0,0 +1,13 @@
+package com.valorrise.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorRiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValorRiseApplication.class, args);
+    }
+
+}
