6ad6529b34da36c9ecc5f6fc2b0c9c564841bfca
rutar <rustarg@gmail.com>
2025-05-27 11:02:31 +0300

    Enable Feign Clients in ValorRiseApplication.

--- src/main/java/com/valorrise/bot/ValorRiseApplication.java
+++ src/main/java/com/valorrise/bot/ValorRiseApplication.java
@@ -2,12 +2,13 @@ package com.valorrise.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.valorrise.bot.api.client")
 public class ValorRiseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ValorRiseApplication.class, args);
     }
-
 }
