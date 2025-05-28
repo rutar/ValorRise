f98d593d8b9f1b58d426ae8415a846f417904770
rutar <rustarg@gmail.com>
2025-05-28 22:17:08 +0300

    Set default property for non-interactive game mode in tests.

--- src/test/java/com/valorrise/bot/ValorRiseApplicationTests.java
+++ src/test/java/com/valorrise/bot/ValorRiseApplicationTests.java
@@ -3,7 +3,7 @@ package com.valorrise.bot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"game.interactive=false"})
 class ValorRiseApplicationTests {
 
     @Test
